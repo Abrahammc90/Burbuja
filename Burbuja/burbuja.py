25587970413af3d905a354045d372b102a75a5ee
Lane Votapka <lvotapka@protonmail.com>
2026-02-19 13:12:25 -0700

    Added additional unit tests for coarse-grained system.

--- Burbuja/burbuja.py
+++ Burbuja/burbuja.py
@@ -122,8 +122,6 @@ def burbuja(
     corner = center_of_geometry_before_wrapping - 0.5 * lengths
     coordinates += -corner[np.newaxis, np.newaxis, :]
     for frame_id in range(n_frames):
-        if frame_id % 2 == 0:
-            print(f"Processing frame {frame_id}/{n_frames}...")
         lengths = base.reshape_atoms_to_orthorombic(
             coordinates, unitcell_vectors, frame_id)
         box_grid = structures.Grid(
